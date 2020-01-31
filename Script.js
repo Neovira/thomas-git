e59532fda420d855c04829bef5d4290282db6d28
Thomas-up <60342325+Thomas-up@users.noreply.github.com>
2020-01-31 12:00:41 +0100

    Partie 1 et 2

--- Script.js
+++ Script.js
@@ -1,3 +1,20 @@
 $( document ).ready(function() {
-    $("p").hide();
+    $("#partie1").hide();
+    $("#partie2").hide();
+
+    $("#but1").click( function () {
+        $(".intro").hide();
+        $("#partie1").show();
+       });
+    
+    $("#but2").click( function () {
+        $("#partie1").hide();
+        $("#partie2").show();
+       });
+    
+    $("#but3").click( function () {
+        $("#partie2").hide();
+        $("#partie3").show();
+       });
+    
 });
